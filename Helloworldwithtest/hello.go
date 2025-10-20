0a8c17905699db50c6a2a44d94ba4bb7c544e386
agnivo988 <agnivamukherjee977@gmail.com>
2025-10-20 12:52:40 +0530

    new tests added

--- Helloworldwithtest/hello.go
+++ Helloworldwithtest/hello.go
@@ -1,15 +1,31 @@
 package main 
 import "fmt"
 
+const bengali = "Bengali"
+const french = "French"
+const spanish = "Spanish"
+const spanishhello = "Hola, "
 const englishHello = "Hello, "
+const frenchhello = "Bonjour, "
+const bengalihello = "Nomoskar, "
 
-func hello(name string) string {
+func hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	if language == spanish {
+		return spanishhello + name
+	}
+    if language == french {
+		return frenchhello + name 
+	}
+	if language == bengali {
+		return bengalihello +name
+	}
+
 	return englishHello + name
 }
 
 func main(){
-	fmt.Println(hello("World"))
+	fmt.Println(hello("World",""))
 }
--- Helloworldwithtest/hello_test.go
+++ Helloworldwithtest/hello_test.go
@@ -20,7 +20,7 @@ Sometimes, it is useful to group tests around a "thing" and then
 
 func TestHello(t *testing.T) {
 	t.Run("say Hello to people",func(t *testing.T){
-		got := hello("Agniva")
+		got := hello("Agniva","")
 		want := "Hello, Agniva"
 		if got != want {
 			t.Errorf("got %q want %q",got,want)
@@ -28,16 +28,34 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t,got,want)
 	})
 	t.Run("say 'Hello, world' when empty string is supplied",func(t *testing.T) {
-		got := hello("")
+		got := hello("","")
 		want := "Hello, World"
 		if got!= want {
 			t.Errorf("got %q want %q",got, want)
 		}
 		assertCorrectMessage(t,got,want)
 	})
+
+	t.Run("say in spanish",func(t *testing.T) {
+		got := hello("Elodie","Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t,got, want)
+	})
+
+	t.Run("say in french", func(t *testing.T) {
+		got := hello("Macron","French")
+		want := "Bonjour, Macron"
+		assertCorrectMessage(t,got,want)
+	})
+
+	t.Run("say in Bengali", func(t *testing.T) {
+		got := hello("Riju","Bengali")
+		want := "Nomoskar, Riju"
+		assertCorrectMessage(t, got , want)
+	})
 }
 
-func assertCorrectMessage(t testing.TB, got , want string) {
+func assertCorrectMessage(t testing.TB, got,want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q",got,want)
